7b91ae4da2c514ffa3d3710c4258b0d674a1126a
Nasrullah Pandu <103873184+pandudw@users.noreply.github.com>
2025-04-26 06:35:28 +0700

    Update main.py

--- aws/ec2-scheduler/main.py
+++ aws/ec2-scheduler/main.py
@@ -2,18 +2,18 @@ import boto3
 import os
 
 def start_instances(ec2, instance_ids):
-     ec2.start_instances(InstanceIds=instance_ids)
-     print(f"Starting EC2 Instance {instance_ids}")
+    ec2.start_instances(InstanceIds=instance_ids)
+    print(f"Starting EC2 Instance {instance_ids}")
 
 def stop_instance(ec2, instance_ids):
     ec2.stop_instances(InstanceIds=instance_ids)
-    print(f"Stopping EC2 Intance {instance_ids}")
+    print(f"Stopping EC2 Instance {instance_ids}")
 
 def lambda_handler(event, context):
-    region = os.getenviron.get("AWS_REGION", "ap-southeast-3")
+    region = os.getenv("AWS_REGION", "ap-southeast-3")
     ec2 = boto3.client("ec2", region_name=region)
-    instance_ids = os.getenviront.get("INSTANCE_IDS", "".split(","))
-    action = get.event("ACTION")
+    instance_ids = os.getenv("INSTANCE_IDS", "").split(",")  
+    action = event.get("ACTION")  
 
     if not instance_ids or instance_ids == [""]:
         print("No EC2 instance ids provided")
